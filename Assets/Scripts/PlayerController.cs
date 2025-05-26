0526c61a981f46c8b15c8d455c5c4c53bdbee3b3
ANaSII3 <s30029@pjwstk.edu.pl>
2025-05-26 12:27:59 +0200

    aa

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float runSpeed = 15;
+    public float jumpForce = 10;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>(); 
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      float moveInput = Input.GetAxis("Horizontal"); 
+      //Debug.Log($"Input pressed: {moveInput}");
+      if(Input.GetKey(KeyCode.LeftShift))
+      {
+
+         rb.velocity = new Vector2(moveInput * runSpeed, rb.velocity.y);
+
+      }
+      
+      
+      else
+      
+      {
+      rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+      }
+      
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+   {
+
+       
+       rb.AddForce( Vector2.up * jumpForce );
+
+
+   }
+ 
+    }
+}
