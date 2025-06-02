a50f7d155559e4cc7a18dde401e2904f9b90d095
ANaSII3 <s30029@pjwstk.edu.pl>
2025-06-02 13:49:03 +0200

    ddd

---
+++ Assets/Scripts/HealthDamager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class HealthDamager : MonoBehaviour
+{
+    public float damage = 40;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionObject = collision.gameObject;
+        
+        HandleTakeDamage(collisionObject);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collisionObject = collision.gameObject;
+        
+        HandleTakeDamage(collisionObject);
+        
+    }
+
+    private void HandleTakeDamage(GameObject collisionObject)
+    {
+        if(!collisionObject.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        PlayerStats stats = collisionObject.gameObject.GetComponent<PlayerStats>();
+
+        if (stats == null)
+        {
+            return;
+        }
+
+        stats.TakeDamage(damage);
+
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+	[SerializeField] private float maxHealth = 100;
+    [SerializeField] private float health = 100;
+	private Animator anim;
+
+	public bool isDead = false;
+
+	private void Start() 
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	public void TakeDamage(float damage)
+	{
+		damage = Mathf.Abs(damage);
+		health = Mathf.Clamp(health - damage, 0, maxHealth);
+		//if health-damage > maxHealth => maxHealth
+		//
+		CheckHealth();
+		
+	}
+
+	public void Heal(float heal)
+	{
+		heal = Mathf.Abs(heal);
+		health = Mathf.Clamp(health + heal, 0, maxHealth);
+		CheckHealth();
+	}
+
+	public void CheckHealth()
+	{
+		if (health == 0)
+		{
+			anim.SetTrigger("isDead");
+			isDead = true;
+		}
+	}
+}
