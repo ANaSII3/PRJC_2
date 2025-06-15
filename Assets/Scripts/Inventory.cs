11f6a01fc1d10d44fe97a4ce0ce56fd4b291ac73
ANaSII3 <s30029@pjwstk.edu.pl>
2025-06-15 21:37:27 +0200

    Update Inventory.cs

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,7 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
-{
 
+	public class Inventory : MonoBehaviour
+{
+    
+    public List<GameObject> items = new List<GameObject>();
+
+   
+    public void AddItem(GameObject item)
+    {
+        items.Add(item);
+        Debug.Log("Предмет добавлен в инвентарь: " + item.name);
+    }
+
+    
 }
